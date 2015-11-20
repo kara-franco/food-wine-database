b506c57b07b074a4eb429f98b64f3431ba1a0cb6
Kara Franco <kara.a.franco@gmail.com>
2015-11-20 10:00:51 -0500

    Updated materials

--- food-wine-DB-selection.sql
+++ food-wine-DB-selection.sql
@@ -18,6 +18,15 @@ INNER JOIN wine ON wine.id=pair.wine_id
 WHERE wine.name = 'Prosecco' 
 ORDER BY food_category ASC;
 
+-- select food(s) by category and name that pairs with a specific wine type -- 
+
+SELECT food_category Category, food.name Name
+FROM food 
+INNER JOIN pair ON food.id=pair.food_id
+INNER JOIN wine ON wine.id=pair.wine_id
+WHERE wine.wine_type = 'Bold Red' 
+ORDER BY food_category ASC;
+
 -- select food(s) by name that will pair with a specific wine and belongs to one of two categories --
 
 SELECT DISTINCT food.food_category Category, food.name Name
@@ -103,6 +112,7 @@ WHERE qry1.id = qry2.id;
 
 SELECT name Name
 FROM wine 
+INNER JOIN is_from ON wine.id=is_from.wine_id
 INNER JOIN region ON wine.region_id=region.id
 WHERE region.country_name = 'France'
 ORDER BY wine_type ASC;
@@ -111,6 +121,7 @@ ORDER BY wine_type ASC;
 
 SELECT temp Temperature 
 FROM wine
+INNER JOIN served_at ON wine.id=served_at.wine_id
 INNER JOIN temperature ON wine.temp_id=temperature.id
 WHERE wine.name = 'Champagne';
 
@@ -119,7 +130,9 @@ WHERE wine.name = 'Champagne';
 SELECT region.country_name Region, grape_variety.grape_name Name
 FROM region 
 INNER JOIN wine ON wine.region_id=region.id
+INNER JOIN is_from ON wine.id=is_from.wine_id
 INNER JOIN grape_variety ON grape_variety.id=wine.variety_id
+INNER JOIN made_from ON grape_variety.id=made_from.grape_id
 WHERE wine.name = 'Merlot';
 
 -- find the wine that has the most pairs -- 
@@ -136,5 +149,6 @@ LIMIT 1;
 SELECT wine.wine_type, wine.name Name
 FROM temperature 
 INNER JOIN wine ON wine.temp_id=temperature.id
+INNER JOIN served_at ON wine.temp_id=served_at.wine_id
 WHERE temperature.temp = '40-50'
 ORDER BY wine_type ASC;
