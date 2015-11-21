64bfd3deb0af9f2f9cf58a07f714723555ce78d2
Kara Franco <kara.a.franco@gmail.com>
2015-11-20 22:12:48 -0500

    Updates

--- food-wine-DB-main.php
+++ food-wine-DB-main.php
@@ -12,66 +12,38 @@ if($mysqli->connect_errno){
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
-  <head>
-    <meta charset="utf-8">
-    <!-- link to style sheet -->
-    <link rel="stylesheet" href="foodWineDB.css" type="text/css">
- 
-    <script></script>  
-   </head>
+<head>
+	<meta charset="utf-8"/>
+	
+	<link rel="stylesheet" type="text/css" href="foodWineDB.css" />
+
+	
+</head>
   <body>
      
-   <!-- ------------------------------------ this starts the form ---------------------------->   
-      <form>
-          
-            <fieldset>
-            <legend class="topLabel"> Perfect Match: A food and wine pairing application     </legend>
+   
+
+           <legend class="topLabel"> Perfect Match: A food and wine pairing application     </legend>
             <br>
-<!-- ------------------------------------ this starts the testing table ---------------------------->       
-                
-                <div>
-	       <table>
-		   <tr>
-			   <td>Food</td>
-		   </tr>
-		       <tr>
-			       <td>Category</td>
-			       <td>Name</td>
-			
-		  </tr>
- <!-- ------------------------------------ php for populating the testing table ---------------------------->           
-<?php
+<br>
+      <br>
 
-if(!($stmt = $mysqli->prepare("SELECT food_category, name FROM food "))){
-	echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
-}
-
-if(!$stmt->execute()){
-	echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
-}
-if(!$stmt->bind_result($category, $name)){
-	echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
-}
-while($stmt->fetch()){
- echo "<tr>\n<td>\n" . $category . "\n</td>\n<td>\n" .  $name . "\n</td>\n</tr>";
-}
-$stmt->close();
-?>
-	</table>
-</div>
-                
   <!-- ------------------------------------ this starts the section for food search  ---------------------------->                     
                 
                              
-     <label for="wineInput">Find food suggestions for your wine:</label>
-                    
+     <label >Find food suggestions for your wine:</label>
+           <br>          
         <br>
                
+   <!-- --------------------------------  searching by wine name ---------------------------->                 
+   <!-- -----------------------  populating select drop down menu for wines---------------------------->               
+   
                 
-   <!-- -------------------------------- php for populating select drop down menu ---------------------------->               
-    <fieldset>
-			<legend>Current Wine List: </legend>
-			<select name="wines">
+<div>
+	<form method="post" action="wineSelect.php">
+                <fieldset>
+			<legend > Search for pairs by your wine name: </legend>
+			<select name="wine">
 <?php
 if(!($stmt = $mysqli->prepare("SELECT id, wine.name FROM wine"))){
 	echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
@@ -90,49 +62,177 @@ $stmt->close();
 ?>
 			</select>
 		</fieldset>
-                
-                
-    <!-- ------------------------------------ this ends the php update!!  ---------------------------->       
-                    <br>
-                <br>
-                
-                   <label for="foodFilter">Filter my menu:</label>
-                <br>
-                   <select name="foodChoices" size="4" multiple >   
-                   <option selected> Cheese
-                   <option> Dessert
-                   <option> Herbs & Spices
-                   <option> Meat
-                  <option> Vegetable
-                  <option> Starch
-                   </select>
-                    <br>
-                   <input type="submit" id="searchSubmit">
-                  <br>
-                  <br>
-                   <label for="foodInput">Find wine suggestions for your menu:</label>
-                    <input type="text" name="foodInput" id="foodInput">
-                       <br>
-                <label for="wineFilter">Filter my wine type:</label>
-                    <br>
-                    <select name="wineChoices" size="4" multiple >   
-                   <option selected> Bold Red
-                   <option> Medium Red
-                   <option> Light Red
-                   <option> Rose
-                  <option> Rich White
-                  <option> Light White
-                <option> Sparkling
-                    <option> Sweet White
-                    <option> Dessert
-                   </select>
-                    <br>
-                 
+     	<input type="submit" value="Find Food" />
+	</form>
+</div>           
+          
+    <!-- --------------------------------  searching by wine type ---------------------------->        
+        <br>
+        <br>
+                      
+<div>
+	<form method="post" action="wine-typeSelect.php">
+                <fieldset>
+			<legend>Search for pairs by your wine type: </legend>
+
+		
+			<p>Wine Type: <input type="text" name="wine_type" /></p>
+			<p> Wine Types include: Bold Red, Medium Red, Light Red, Rose, Rich White,  Light White, Sparkling, Sweet White and Dessert  </p>
+	
                     
-                    <input type="submit" id="searchSubmit">
-                    <br>
-             
-            </fieldset>
-        </form>      
+		</fieldset>
+     	<input type="submit" value="Find Food" />
+	</form>
+</div>           
+          
+    <!-- ------------------------------------ searching by   ---------------------------->       
+      <br>          
+        <br>      
+              
+       <!-- ------------------------------------ this starts the section for wine search  ---------------------------->  
+       <!-- ------------------------------------ search by food name ---------------------------->  
+         <label>Find wine suggestions for your menu:</label>
+           <br>          
+        <br>      
+                   
+<div>
+	<form method="post" action="foodSelect.php">
+                <fieldset>
+			<legend>Search for pairs by food name: </legend>
+			<select name="food">
+<?php
+if(!($stmt = $mysqli->prepare("SELECT id, food.name FROM food"))){
+	echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+}
+
+if(!$stmt->execute()){
+	echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+if(!$stmt->bind_result($id, $name)){
+	echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+while($stmt->fetch()){
+	echo '<option value=" '. $id . ' "> ' . $name . '</option>\n';
+}
+$stmt->close();
+?>
+			</select>
+		</fieldset>
+     	<input type="submit" value="Find Wine" />
+	</form>
+</div>      
+      
+    <!-- ------------------------------------ search by two food names ---------------------------->     
+      
+        <br>
+        <br>
+                      
+<div>
+	<form method="post" action="two-foodSelect.php">
+                <fieldset>
+			<legend>Search for pairs by two food names: </legend>
+
+		
+			<p>Name 1: <input type="text" name="food_one" /></p>
+			<p>Name 2: <input type="text" name="food_two" /></p>
+            <p> Food Names can be found in documentation  </p>
+	
+                    
+		</fieldset>
+     	<input type="submit" value="Find Wine" />
+	</form>
+</div> 
+      
+    <!-- ------------------- insert wine section ------------------------------------ -->
+          <br>          
+        <br>
+          <label >Can't find your wine? Input is into our database!</label>
+           <br>          
+        <br>  
+      <div>
+	<form method="post" action="addWine.php"> 
+
+		<fieldset>
+			<legend>Name</legend>
+			<p>Wine Name: <input type="text" name="name" /></p>
+			<p>Wine Type: <input type="text" name="wine_type" /></p>
+		</fieldset>
+
+		<fieldset>
+			<legend>Region</legend>
+			<select name="region">
+<?php
+if(!($stmt = $mysqli->prepare("SELECT id, country_name FROM region"))){
+	echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+}
+
+if(!$stmt->execute()){
+	echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+if(!$stmt->bind_result($id, $country_name)){
+	echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+while($stmt->fetch()){
+	echo '<option value=" '. $id . ' "> ' . $country_name . '</option>\n';
+}
+$stmt->close();
+?>
+			</select>
+		</fieldset>
+		
+        		<fieldset>
+			<legend>Grape Variety</legend>
+			<select name="grape">
+<?php
+if(!($stmt = $mysqli->prepare("SELECT id, grape_name FROM grape_variety"))){
+	echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+}
+
+if(!$stmt->execute()){
+	echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+if(!$stmt->bind_result($id, $grape_name)){
+	echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+while($stmt->fetch()){
+	echo '<option value=" '. $id . ' "> ' . $grape_name . '</option>\n';
+}
+$stmt->close();
+?>
+			</select>
+		</fieldset>
+        
+        
+                		<fieldset>
+			<legend>Serving Temperature</legend>
+			<select name="temp">
+<?php
+if(!($stmt = $mysqli->prepare("SELECT id, temp FROM temperature"))){
+	echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+}
+
+if(!$stmt->execute()){
+	echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+if(!$stmt->bind_result($id, $temp)){
+	echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+while($stmt->fetch()){
+	echo '<option value=" '. $id . ' "> ' . $temp . '</option>\n';
+}
+$stmt->close();
+?>
+			</select>
+		</fieldset>
+        
+        
+        
+        
+        
+        <p><input type="submit" /></p>
+	</form>
+</div>
+      
+       
   </body>
 </html>
